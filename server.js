043ae8c94df1337f4a1cfb5b51e85bc8fb5fa3c6
Young <dndud5182@gmail.com>
2025-05-15 22:02:47 +0900

    Feat : 서버 포트 설정을 환경 변수로 변경

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 3000;
+
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
+
+server.listen(PORT, () => {
     console.log("JSON Server is running");
 });
